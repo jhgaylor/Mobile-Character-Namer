69b4945808dbec2362e101e2a474c603498244d1
Jake Gaylor <jhgaylor@gmail.com>
2012-12-28 20:51:41 -0600

    patched everything together dynamically

--- CharacterNamer/Namer/views.py
+++ CharacterNamer/Namer/views.py
@@ -1,1 +1,7 @@
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 # Create your views here.
+
+def render_app(request):
+	data = {}
+	return render_to_response('app.html', RequestContext(request, data))
